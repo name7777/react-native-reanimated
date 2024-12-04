258891e5c230e03becb47014ebaf2cfe5c5fec8f
EthanKwon <mywonhyuni@gmail.com>
2024-12-04 23:47:44 +0900

    feat: repeat, backgorund > reanimated 적용

--- components/pass-code/index.tsx
+++ components/pass-code/index.tsx
@@ -1,4 +1,4 @@
-import { useCallback, useState } from 'react'
+import { useCallback, useEffect, useState } from 'react'
 import { FlatList, StyleSheet, View } from 'react-native'
 
 import AnimatedEnteredCode from './ui/animated-entered-code'
@@ -7,23 +7,44 @@ import { NumberPad } from './ui/number-pad'
 const CORRECT_CODE = [1, 2, 3, 4]
 
 export function PassCode() {
-  const [code, setCode] = useState<number[]>([])
+  const [passCode, setPassCode] = useState<number[]>([])
+  const [isCorrect, setIsCorrect] = useState(false)
 
   const onPressKey = (num: number | string) => {
     if (typeof num === 'string') {
-      setCode(prev => prev.slice(0, -1))
+      setPassCode(prev => prev.slice(0, -1))
     } else {
-      setCode(prev => [...prev, num])
+      if (passCode.length === CORRECT_CODE.length) {
+        return
+      }
+      setPassCode(prev => [...prev, num])
     }
   }
 
-  const renderItem = useCallback(({ item }: { item: number }) => {
-    return <AnimatedEnteredCode item={item} />
-  }, [])
+  const renderItem = useCallback(
+    ({ item }: { item: number }) => {
+      const isFullCode = passCode.length === CORRECT_CODE.length
+      return <AnimatedEnteredCode item={item} isCorrect={isCorrect} isFullCode={isFullCode} />
+    },
+    [passCode, isCorrect],
+  )
+
+  useEffect(() => {
+    if (passCode.length === CORRECT_CODE.length) {
+      const isCorrect = passCode.every((item, index) => item === CORRECT_CODE[index])
+
+      setIsCorrect(isCorrect)
+    }
+  }, [passCode, isCorrect])
 
   return (
     <View style={styles.container}>
-      <FlatList contentContainerStyle={styles.contentContainerStyle} data={code} renderItem={renderItem} horizontal />
+      <FlatList
+        contentContainerStyle={styles.contentContainerStyle}
+        data={passCode}
+        renderItem={renderItem}
+        horizontal
+      />
       <NumberPad onPressKey={onPressKey} />
     </View>
   )
--- components/pass-code/ui/animated-entered-code.tsx
+++ components/pass-code/ui/animated-entered-code.tsx
@@ -1,19 +1,26 @@
-import { memo } from 'react'
 import { StyleSheet, Text, View } from 'react-native'
-import Animated, { useAnimatedStyle, useSharedValue, withSpring, withTiming } from 'react-native-reanimated'
+import Animated, { useAnimatedStyle, useSharedValue, withRepeat, withSpring, withTiming } from 'react-native-reanimated'
 
-function AnimatedEnteredCode({ item }: { item: any }) {
+function AnimatedEnteredCode({ item, isCorrect, isFullCode }: { item: any; isCorrect: boolean; isFullCode: boolean }) {
   const scale = useSharedValue(0)
+  const repeat = useSharedValue(0)
+  const backgroundColor = useSharedValue('purple')
 
   scale.value = withSpring(1, { duration: 500 })
+  repeat.value = !isCorrect && isFullCode ? withRepeat(withTiming(5, { duration: 100 }), 6, true) : 0
+  backgroundColor.value =
+    isCorrect && isFullCode ? withTiming('green', { duration: 500 }) : withTiming('purple', { duration: 500 })
 
-  const animatedStyle = useAnimatedStyle(() => ({
-    width: 50,
-    height: 50,
-    borderRadius: 25,
-    backgroundColor: 'purple',
-    transform: [{ scale: scale.value }],
-  }))
+  const animatedStyle = useAnimatedStyle(
+    () => ({
+      width: 50,
+      height: 50,
+      borderRadius: 25,
+      backgroundColor: backgroundColor.value,
+      transform: [{ scale: scale.value }, { translateX: repeat.value }],
+    }),
+    [isFullCode, isCorrect],
+  )
 
   return (
     <Animated.View style={animatedStyle}>
@@ -38,4 +45,4 @@ const styles = StyleSheet.create({
   },
 })
 
-export default memo(AnimatedEnteredCode)
+export default AnimatedEnteredCode
